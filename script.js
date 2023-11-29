88d7f4779b17ffff27dc6aa32a42e34be5213782
Artem <artem256korolev@gmail.com>
2023-11-29 15:47:02 +0300

    add block restaurants

---
+++ script.js
@@ -0,0 +1,32 @@
+const favorites = () => {
+    const favoritesBtn = document.querySelectorAll('.favourites'),
+        favoritesContent = document.querySelectorAll('.favorites-circle')
+
+        favoritesBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                favoritesContent.forEach(content => {
+                    content.classList.toggle('active')
+                })
+            })
+        })
+}
+
+favorites()
+
+const addBlockRest = () => {
+    const openBtn = document.querySelectorAll('.rest-open-btn'),
+        block = document.querySelector('.rest-block-close'),
+        arrow = document.querySelectorAll('.rest-open-img')
+
+        openBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                block.classList.toggle('active')
+
+                arrow.forEach(content => {
+                    content.classList.toggle('active')
+                })
+            })
+        })
+}
+
+addBlockRest()
