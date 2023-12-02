7a0b97faeb80656ac6875a8db47e38addd7d9be9
Artem <artem256korolev@gmail.com>
2023-12-03 00:58:08 +0300

    add block purchases

--- script.js
+++ script.js
@@ -39,3 +39,23 @@ const addBlockView = () => {
 }
 
 addBlockView()
+
+const dropDown = () => {
+    const dropDownBtn = document.querySelector('.purchases-content-btn-wrapper'),
+        dropDownItems = document.querySelector('.purchases-content-select'),
+        arrow = document.querySelector('.purchases-content-btn-arrow'),
+        text = document.querySelector('.purchases-content-btn-text'),
+        selectText = document.querySelectorAll('.purchases-content-btn-select')
+
+            dropDownBtn.addEventListener('click', () => {
+                dropDownItems.classList.toggle('active')
+                arrow.classList.toggle('active')
+                selectText.forEach(item => {
+                    item.addEventListener('click', () => {
+                        text.textContent = item.textContent
+                    })
+                })
+            })
+}
+
+dropDown()
