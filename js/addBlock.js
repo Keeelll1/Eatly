3ae9b50443fbab629a1e1b03e91aee80cfa0bd73
Artem <artem256korolev@gmail.com>
2023-12-13 14:32:58 +0300

    add distribution by files

--- js/addBlock.js
+++ js/addBlock.js
@@ -39,35 +39,3 @@ const addBlockView = () => {
 }
 
 addBlockView()
-
-const dropDown = () => {
-    const dropDownBtn = document.querySelector('.purchases-content-btn-wrapper'),
-        dropDownItems = document.querySelector('.purchases-content-select'),
-        arrow = document.querySelector('.purchases-content-btn-arrow'),
-        text = document.querySelector('.purchases-content-btn-text'),
-        selectText = document.querySelectorAll('.purchases-content-btn-select')
-
-            dropDownBtn.addEventListener('click', () => {
-                dropDownItems.classList.toggle('active')
-                arrow.classList.toggle('active')
-                selectText.forEach(item => {
-                    item.addEventListener('click', () => {
-                        text.textContent = item.textContent
-                    })
-                })
-            })
-}
-
-dropDown()
-
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    slidesPerView: 1.3,
-    centeredSlides: false,
-    spaceBetween: 40,
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true
-    }
-  });
